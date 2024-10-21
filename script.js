8f446481c4919aa213f5bbd55329df2d0ca9db5a
DismissedGv <129997550+DismissedGv@users.noreply.github.com>
2024-10-21 16:51:35 +0200

    update

--- script.js
+++ script.js
@@ -7,24 +7,3 @@ function closemenu()
 {
     sidemenu.style.right = "-200px";
 }
-
-//-----------------------------------------------------------------Navigation bar
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 200;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-        
-        if(top >= offset && top < offset + height)
-        {
-            navLinks.forEach(links => {
-                links.classList.remove('active-link');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active-link');
-            });
-        };
-    });
-};
